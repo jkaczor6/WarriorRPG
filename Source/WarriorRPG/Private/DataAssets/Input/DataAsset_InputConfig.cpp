b5f167c7430ee09f651dc80a02fb377bdb08bb60
Jakub Kaczor <j.kaczor06@gmail.com>
2026-06-09 18:16:01 +0200

    Add Input Config Data Asset

---
+++ Source/WarriorRPG/Private/DataAssets/Input/DataAsset_InputConfig.cpp
@@ -0,0 +1,17 @@
+// Copyright Jakub Kaczor
+
+
+#include "DataAssets/Input/DataAsset_InputConfig.h"
+
+UInputAction* UDataAsset_InputConfig::FindNativeInputActionByTag(const FGameplayTag& InInputTag)
+{
+	for (const FWarriorRPGInputActionConfig& InputActionConfig : NativeInputActions )
+	{
+		if (InputActionConfig.InputTag == InInputTag && InputActionConfig.InputAction)
+		{
+			return InputActionConfig.InputAction;
+		}
+	}
+	
+	return nullptr;
+}
---
+++ Source/WarriorRPG/Public/DataAssets/Input/DataAsset_InputConfig.h
@@ -0,0 +1,38 @@
+// Copyright Jakub Kaczor
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "InputAction.h"
+#include "Engine/DataAsset.h"
+#include "DataAsset_InputConfig.generated.h"
+
+class UInputMappingContext;
+
+USTRUCT(BlueprintType)
+struct FWarriorRPGInputActionConfig
+{
+	GENERATED_BODY()
+		
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (Categories = "InputTag"))
+	FGameplayTag InputTag;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TObjectPtr<UInputAction> InputAction;
+};
+UCLASS()
+class WARRIORRPG_API UDataAsset_InputConfig : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TObjectPtr<UInputMappingContext> DefaultMappingContext;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (TitleProperty = "InputTag"))
+	TArray<FWarriorRPGInputActionConfig> NativeInputActions;
+	
+	UInputAction* FindNativeInputActionByTag(const FGameplayTag& InInputTag);
+};
