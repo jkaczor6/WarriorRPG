a3222b0c9556b79e5967a848f93a1d25831e8c55
Jakub Kaczor <j.kaczor06@gmail.com>
2026-06-09 17:56:39 +0200

    Set up Hero Components

--- Source/WarriorRPG/Private/Characters/WarriorRPGHeroCharacter.cpp
+++ Source/WarriorRPG/Private/Characters/WarriorRPGHeroCharacter.cpp
@@ -3,6 +3,34 @@
 
 #include "Characters/WarriorRPGHeroCharacter.h"
 #include "WarriorRPGDebugHelper.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+AWarriorRPGHeroCharacter::AWarriorRPGHeroCharacter()
+{
+	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.f);
+	
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+	
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>("CameraBoom");
+	CameraBoom->SetupAttachment(RootComponent);
+	CameraBoom->TargetArmLength = 200.f;
+	CameraBoom->SocketOffset = FVector(0.f, 55.f, 65.f);
+	CameraBoom->bUsePawnControlRotation = true;
+	
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>("FollowCamera");
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	FollowCamera->bUsePawnControlRotation = false;
+	
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 500.0f, 0.0f);
+	GetCharacterMovement()->MaxWalkSpeed = 400.f;
+	GetCharacterMovement()->BrakingDecelerationWalking = 2000.f;
+}
 
 void AWarriorRPGHeroCharacter::BeginPlay()
 {
--- Source/WarriorRPG/Public/Characters/WarriorRPGHeroCharacter.h
+++ Source/WarriorRPG/Public/Characters/WarriorRPGHeroCharacter.h
@@ -6,14 +6,25 @@
 #include "Characters/WarriorRPGBaseCharacter.h"
 #include "WarriorRPGHeroCharacter.generated.h"
 
-/**
- * 
- */
+class USpringArmComponent;
+class UCameraComponent;
+
 UCLASS()
 class WARRIORRPG_API AWarriorRPGHeroCharacter : public AWarriorRPGBaseCharacter
 {
 	GENERATED_BODY()
 	
+public:
+	AWarriorRPGHeroCharacter();
 protected:
 	virtual void BeginPlay();
+	
+private:
+#pragma region Components
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = true))
+	TObjectPtr<USpringArmComponent> CameraBoom;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = true))
+	TObjectPtr<UCameraComponent> FollowCamera;
+#pragma endregion
 };
