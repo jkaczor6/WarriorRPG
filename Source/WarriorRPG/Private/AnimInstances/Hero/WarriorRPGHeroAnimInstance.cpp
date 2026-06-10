37c6b3086b412ad3948d9b6f3e57fd97dd41b14f
Jakub Kaczor <j.kaczor06@gmail.com>
2026-06-10 20:11:02 +0200

    Add Hero Relax State animation

--- Source/WarriorRPG/Private/AnimInstances/Hero/WarriorRPGHeroAnimInstance.cpp
+++ Source/WarriorRPG/Private/AnimInstances/Hero/WarriorRPGHeroAnimInstance.cpp
@@ -3,3 +3,31 @@
 
 #include "AnimInstances/Hero/WarriorRPGHeroAnimInstance.h"
 
+#include "Characters/WarriorRPGHeroCharacter.h"
+
+void UWarriorRPGHeroAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+	
+	if (OwningCharacter)
+	{
+		OwningHeroCharacter = Cast<AWarriorRPGHeroCharacter>(OwningCharacter);
+	}
+	
+}
+
+void UWarriorRPGHeroAnimInstance::NativeThreadSafeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeThreadSafeUpdateAnimation(DeltaSeconds);
+	
+	if (bHasAcceleration)
+	{
+		IdleElapsedTime = 0.f;
+		bShouldEnterRelaxState = false;
+	}
+	else
+	{
+		IdleElapsedTime += DeltaSeconds;
+		bShouldEnterRelaxState = (IdleElapsedTime >= EnterRelaxStateThreshold);
+	}
+}
--- Source/WarriorRPG/Public/AnimInstances/Hero/WarriorRPGHeroAnimInstance.h
+++ Source/WarriorRPG/Public/AnimInstances/Hero/WarriorRPGHeroAnimInstance.h
@@ -6,9 +6,26 @@
 #include "AnimInstances/WarriorRPGCharacterAnimInstance.h"
 #include "WarriorRPGHeroAnimInstance.generated.h"
 
+class AWarriorRPGHeroCharacter;
+
 UCLASS()
 class WARRIORRPG_API UWarriorRPGHeroAnimInstance : public UWarriorRPGCharacterAnimInstance
 {
 	GENERATED_BODY()
 	
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeThreadSafeUpdateAnimation(float DeltaSeconds) override;
+	
+protected:
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "AnimData|Refrences")
+	TObjectPtr<AWarriorRPGHeroCharacter> OwningHeroCharacter;
+	
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "AnimData|LocomotionData")
+	bool bShouldEnterRelaxState;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "AnimData|LocomotionData")
+	float EnterRelaxStateThreshold{ 5.f };
+	
+	float IdleElapsedTime;
 };
