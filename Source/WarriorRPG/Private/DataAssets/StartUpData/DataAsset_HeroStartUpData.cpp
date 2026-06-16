e307b047facb161a9aba9b1c15a45dba097a9ac7
Jakub Kaczor <j.kaczor06@gmail.com>
2026-06-16 12:47:58 +0200

    Add Hero Ability Set

--- Source/WarriorRPG/Private/DataAssets/StartUpData/DataAsset_HeroStartUpData.cpp
+++ Source/WarriorRPG/Private/DataAssets/StartUpData/DataAsset_HeroStartUpData.cpp
@@ -2,4 +2,28 @@
 
 
 #include "DataAssets/StartUpData/DataAsset_HeroStartUpData.h"
+#include "AbilitySystem/Abilities/WarriorRPGGameplayAbility.h"
+#include "AbilitySystem/WarriorRPGAbilitySystemComponent.h"
 
+bool FWarriorRPGHeroAbilitySet::IsValid() const
+{
+	return InputTag.IsValid() && AbilityToGrant;
+}
+
+void UDataAsset_HeroStartUpData::GiveToAbilitySystemComponent(UWarriorRPGAbilitySystemComponent* InASCToGive,
+	int32 ApplyLevel)
+{
+	Super::GiveToAbilitySystemComponent(InASCToGive, ApplyLevel);
+	
+	for (const FWarriorRPGHeroAbilitySet& AbilitySet : HeroStartUpAbilitySets)
+	{
+		if (!AbilitySet.IsValid()) continue;
+		
+		FGameplayAbilitySpec AbilitySpec(AbilitySet.AbilityToGrant);
+		AbilitySpec.SourceObject = InASCToGive->GetAvatarActor();
+		AbilitySpec.Level = ApplyLevel;
+		AbilitySpec.DynamicAbilityTags.AddTag(AbilitySet.InputTag);
+		
+		InASCToGive->GiveAbility(AbilitySpec);
+	}
+}
--- Source/WarriorRPG/Public/DataAssets/StartUpData/DataAsset_HeroStartUpData.h
+++ Source/WarriorRPG/Public/DataAssets/StartUpData/DataAsset_HeroStartUpData.h
@@ -4,7 +4,22 @@
 
 #include "CoreMinimal.h"
 #include "DataAssets/StartUpData/DataAsset_StartUpDataBase.h"
+#include "GameplayTagContainer.h"
 #include "DataAsset_HeroStartUpData.generated.h"
+
+USTRUCT(BlueprintType)
+struct FWarriorRPGHeroAbilitySet
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (Categories = "InputTag"))
+	FGameplayTag InputTag;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TSubclassOf<UWarriorRPGGameplayAbility> AbilityToGrant;
+	
+	bool IsValid() const;
+};
 
 /**
  * 
@@ -14,4 +29,10 @@ class WARRIORRPG_API UDataAsset_HeroStartUpData : public UDataAsset_StartUpDataB
 {
 	GENERATED_BODY()
 	
+public:
+	virtual void GiveToAbilitySystemComponent(UWarriorRPGAbilitySystemComponent* InASCToGive, int32 ApplyLevel = 1) override;
+	
+private:
+	UPROPERTY(EditDefaultsOnly, Category="StartUpData", meta = (TitleProperty = "InputTag"))
+	TArray<FWarriorRPGHeroAbilitySet> HeroStartUpAbilitySets;
 };
